e1f05990cd6607fba001299f3b1c3fe152dd8e64
Greg Heo <greg@gregheo.com>
2015-01-19 14:45:37 -0500

    Add Core Data stuff to the starter project

--- 1-Starter/RWDevCon Starter/RWDevCon WatchKit Extension/ScheduleInterfaceController.swift
+++ 1-Starter/RWDevCon Starter/RWDevCon WatchKit Extension/ScheduleInterfaceController.swift
@@ -2,11 +2,19 @@ import WatchKit
 import Foundation
 
 class ScheduleInterfaceController: WKInterfaceController {
+  lazy var coreDataStack = CoreDataStack()
+  var sessions = [Session]()
 
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
 
     // Configure interface objects here.
+  }
+
+  func showAllSessions() {
+    sessions = Session.allSessionsInContext(coreDataStack.context)
+
+    // Configure the display of all sessions here.
   }
 
   override func willActivate() {
